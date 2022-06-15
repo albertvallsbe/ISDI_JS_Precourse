f544daf3bb52f90128f794be8480269f482d3388
Albert <avalls89@gmail.com>
2022-06-15 20:21:53 +0200

    15/6/22

--- 1.-Variables/ISDI_Variables_7.js
+++ 1.-Variables/ISDI_Variables_7.js
@@ -1,7 +1,9 @@
-const numbers = [2, 9, 6, 7, 8];
+const numbers1 = [2, 9, 6, 7, 8];
 let message = "We are in the middle of loop";
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-   
+for (let i = 0; i < numbers1.length; i++) {
+    console.log(numbers1[i]);
+    if (i == 2) {
+        console.log(message);
+    }
 }
