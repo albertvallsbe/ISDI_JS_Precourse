2d1753fb9aa288384d20491e45c15ab0e2fa942d
Albert <avalls89@gmail.com>
2022-08-23 20:36:02 +0200

    23/8/22

---
+++ C.-CalcularoraDOM_J/calculadora.js
@@ -0,0 +1,91 @@
+function Accion(x){
+    document.getElementById('resultado').innerHTML +=x;
+}
+
+function limpiarPantalla(){
+    document.getElementById('resultado').innerHTML = "";
+}
+function calculateResultado(){
+    let resultado = eval(document.getElementById('resultado').innerHTML);
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+let cero = document.getElementById('cero');
+let uno = document.getElementById('uno');
+let dos = document.getElementById('dos');
+let tres = document.getElementById('tres');
+let cuatro = document.getElementById('cuatro');
+let cinco = document.getElementById('cinco');
+let seis = document.getElementById('seis');
+let siete = document.getElementById('siete');
+let ocho = document.getElementById('ocho');
+let nueve = document.getElementById('nueve');
+let coma = document.getElementById('coma');
+
+let suma = document.getElementById('suma');
+let resta = document.getElementById('resta');
+let multiplicacion = document.getElementById('multiplicacion');
+let division = document.getElementById('division');
+let porcentaje = document.getElementById('porcentaje');
+let inverso = document.getElementById('inverso');
+let igual = document.getElementById('igual');
+let borrar = document.getElementById('borrar');
+
+coma.addEventListener("click", function(){
+    Accion(".");
+})
+cero.addEventListener("click", function(){
+    Accion(0);
+})
+uno.addEventListener("click", function(){
+    Accion(1);
+})
+dos.addEventListener("click", function(){
+    Accion(2);
+})
+tres.addEventListener("click", function(){
+    Accion(3);
+})
+cuatro.addEventListener("click", function(){
+    Accion(4);
+})
+cinco.addEventListener("click", function(){
+    Accion(5);
+})
+seis.addEventListener("click", function(){
+    Accion(6);
+})
+siete.addEventListener("click", function(){
+    Accion(7);
+})
+ocho.addEventListener("click", function(){
+    Accion(8);
+})
+nueve.addEventListener("click", function(){
+    Accion(9);
+})
+
+suma.addEventListener("click", function(){
+    Accion("+");
+})
+resta.addEventListener("click", function(){
+    Accion("-");
+})
+multiplicacion.addEventListener("click", function(){
+    Accion("*");
+})
+division.addEventListener("click", function(){
+    Accion("/");
+})
+porcentaje.addEventListener("click", function(){
+    Accion("%");
+})
+inverso.addEventListener("click", function(){
+    Accion("+/-");
+})
+igual.addEventListener("click", function(){
+    calculateResultado();
+})
+borrar.addEventListener("click", function(){
+    limpiarPantalla();
+})
--- C.-ClassesPreCourse/Classe3.js
+++ C.-ClassesPreCourse/Classe3.js
@@ -2,12 +2,17 @@ const word = "codigo";
 const wordToGuess = [];
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const usedNumbers = [];
+let rounds = 0;
+let won = false;
+let isPlaying = true;
+
+
 
 const generateWordToGuess = () => {
     for (let i = 0; i < word.length; i++) {
         wordToGuess.push({ letter: word[i], isGuessed: false });
-    }
-}
+    };
+};
 
 const getRandomLetter = () => {
     const randomNumber = Math.floor(Math.random() * alphabet.length);
@@ -15,22 +20,57 @@ const getRandomLetter = () => {
 
     if (usedNumbers.includes(randomNumber)) {
         return getRandomLetter();
-    }
+    };
 
     usedNumbers.push(randomNumber);
     return alphabet.charAt(randomNumber);
-}
+};
 
 const newTurn = () => {
     const randomLetter = getRandomLetter();
-    alert(`Your letter is: ${randomLetter.toUppercase()}`);
+    alert(`Your letter is: ${randomLetter.toUpperCase()}`);
 
-    wordToGuess.forEach(character=> {
-        if(character.letter === randomLetter ){
+    wordToGuess.forEach(character => {
+        if (character.letter === randomLetter) {
             character.isGuessed = true;
-        }
+        };
     });
+
+    rounds++;
+};
+
+const checkIfWin = () => {
+    for (let i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i].isGuessed === false) {
+            return;
+        }
+    }
+
+    won = true;
+    return;
 }
 
-generateWordToGuess();
-console.table(wordToGuess);
+const playGame = () => {
+    do {
+        if (confirm("Do you want a letter? 8-) ")) {
+            newTurn();
+            checkIfWin();
+            console.table(wordToGuess);
+        } else {
+            isPlaying = false;
+        }
+    } while (!won && isPlaying);
+    if (won) {
+        alert(` Congratulations! You Won in ${rounds} rounds`);
+    } else {
+        alert(" Goodbye! 8-(");
+    }
+}
+
+const letsPlay = () => {
+    alert(" Hello! Welcome to THE GAME! ");
+    generateWordToGuess();
+    playGame();
+};
+
+letsPlay();
