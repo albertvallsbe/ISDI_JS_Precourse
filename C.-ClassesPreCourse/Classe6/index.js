9c46724126ac9d769863b184362cc88c40dce91b
Albert Valls <avalls89@gmail.com>
2022-09-08 13:08:53 +0200

    08/09/22

--- C.-ClassesPreCourse/Classe6/index.js
+++ C.-ClassesPreCourse/Classe6/index.js
@@ -1,12 +1,24 @@
 console.log('Loaded index');
 
+//Seleccionar elemento del DOM
 const elementBody = document.querySelector('body');
-
 console.dir(elementBody);
-
 const elementP = document.querySelector('.highlighted');
-
 console.dir(elementP);
 
+//elementP.innerHTML += ' - Esto no estaba';
 
-elementP.innerHTML += ' - Esto no estaba';
+const elementInputName = document.querySelector('#user-name');
+
+//Leer el contenido de los elementos del DOM 
+const initialElementP = elementP.innerHTML;
+
+let userName;
+
+//Responder a eventos del DOM
+elementInputName.addEventListener("input", () => {
+    userName = elementInputName.value;
+    //Modificar elementos del DOM
+    elementP.innerHTML = initialElementP + " - " + userName;
+
+})
